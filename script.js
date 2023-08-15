afb41840f52e620f5db84a18d93caf290bf44253
RavinduBA <e19004@eng.pdn.ac.lk>
2023-08-15 23:12:38 +0530

    further sections created

---
+++ script.js
@@ -0,0 +1,12 @@
+// Javascript for toggle menu-
+
+
+
+var navLinks =document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right= "-200px";
+}
